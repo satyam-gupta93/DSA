745ade2eafd1202a445e4067b77dceb679da0131
Satyam Gupta <satyamgupta9302@gmail.com>
2026-01-12 19:05:05 +0530

    Space Otiminzed solution of countSquareDP

--- 15_Dynamic_Programming/318_CountSquareSubmatrix_DP.cpp
+++ 15_Dynamic_Programming/318_CountSquareSubmatrix_DP.cpp
@@ -1,8 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-int countSquares(vector<vector<int>>& matrix) {
+/*
+   1: Tabulation (2D DP)
+   TC: O(n*m)
+   SC: O(n*m)
+*/
+int countSquaresDP(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
 
@@ -31,6 +35,46 @@ int countSquares(vector<vector<int>>& matrix) {
        return sum;
 }
 
+/* 
+   2: Space Optimized DP
+   TC: O(n*m)
+   SC: O(m)
+ */
+int countSquaresSpaceOptimized(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+       
+        vector<int>prev(m,0); 
+         int sum = 0;
+        for(int j = 0; j<m; j++){
+            prev[j] = matrix[0][j];
+            sum += matrix[0][j];
+        }
+       
+       
+        for(int i = 1; i<n;i++){
+            
+            vector<int>curr(m,0); 
+            for(int j = 0; j<m; j++){
+                if(j == 0){
+                    curr[0] = matrix[i][0];
+                    sum+=matrix[i][0];
+                    continue;
+                }
+                if(matrix[i][j] == 1){
+                    curr[j] = 1 + min(prev[j],min(curr[j-1],prev[j-1]));
+                    sum += curr[j];
+                }
+                
+               
+            }
+            prev = curr;
+        }
+        
+       return sum;
+}
+
 
 int main() {
     vector<vector<int>> matrix = {
@@ -39,7 +83,9 @@ int main() {
         {0, 1, 1, 1}
     };
 
-    cout <<"Total square submatrices with all 1s:"<<countSquares(matrix) << endl;
+    cout << "2D DP Answer:"<< countSquaresDP(matrix) << endl;
+
+    cout << "Space Optimized Answer:"<< countSquaresSpaceOptimized(matrix) << endl;
 
     return 0;
 }
